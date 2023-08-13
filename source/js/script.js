119ff22c540f8ebf117c5f0a9e05dcc093b5780e
Anastasia <mrgabrielripper@gmail.com>
2023-08-13 23:53:09 +0600

    Начинает исправления для меню навигации, добавляет скрипты

---
+++ source/js/script.js
@@ -0,0 +1,20 @@
+const siteMenu = document.querySelector('.site-menu'),
+      menuToggle = document.querySelector('.main-header__toggle');
+
+
+siteMenu.classList.remove('site-menu--nojs');
+siteMenu.classList.remove('site-menu--opened');
+
+menuToggle.addEventListener('click', () => {
+  if (siteMenu.classList.contains('site-menu--opened')) {
+    siteMenu.classList.add('site-menu--closed');
+    siteMenu.classList.remove('site-menu--opened');
+    menuToggle.classList.remove("main-header__toggle--opened");
+    console.log("Closed");
+  } else {
+    siteMenu.classList.remove('site-menu--closed');
+    siteMenu.classList.add('site-menu--opened');
+    menuToggle.classList.add("main-header__toggle--opened");
+    console.log("Opened");
+  }
+})

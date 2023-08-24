0a37490f0acdf5cd5a9e674ad81a46826a38bc54
Anastasia <mrgabrielripper@gmail.com>
2023-08-24 23:21:31 +0600

    Добавлены адаптивные изображения, завершен слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,70 @@
+// Навигационное меню
 const siteMenu = document.querySelector('.site-menu'),
       menuToggle = document.querySelector('.menu-toggle');
 
+const clearNoJs = () => {
+  siteMenu.classList.remove('site-menu--nojs');
+  menuToggle.classList.remove('menu-toggle--nojs');
+  siteMenu.classList.remove('site-menu--opened');
+}
 
-siteMenu.classList.remove('site-menu--opened');
-
-menuToggle.addEventListener('click', () => {
+const toggleMenuFunc = () => {
   if (siteMenu.classList.contains('site-menu--opened')) {
-    siteMenu.classList.add('site-menu--closed');
-    siteMenu.classList.remove('site-menu--opened');
-    menuToggle.classList.remove("menu-toggle--opened");
+    siteMenu.classList.toggle('site-menu--opened');
+    menuToggle.classList.toggle("menu-toggle--opened");
   } else {
-    siteMenu.classList.remove('site-menu--closed');
-    siteMenu.classList.add('site-menu--opened');
-    menuToggle.classList.add("menu-toggle--opened");
+    siteMenu.classList.toggle('site-menu--opened');
+    menuToggle.classList.toggle("menu-toggle--opened");
   }
-})
+}
+
+clearNoJs();
+menuToggle.addEventListener('click', toggleMenuFunc);
+
+// Слайдер
+const sliderWrapper = document.querySelector('.comparison-slider__image-wrapper'),
+      sliderImageBefore = document.querySelector('.comparison-slider__image--before'),
+      sliderImageAfter = document.querySelector('.comparison-slider__image--after'),
+      sliderToggle = document.querySelector('.comparison-slider__toggle');
+
+// Через деструктуризацию сохраняем значения нужных свойств полученных от метода Web API
+const {x: containerX, width: containerWidth} = sliderWrapper.getBoundingClientRect();
+
+const handleDown = (event) => {
+  // Прослушиваем события движения и опускание кнопки мыши
+  window.addEventListener('mousemove', handleDrag);
+  window.addEventListener('mouseup', handleMouseUp);
+}
+
+const handleMouseUp = () => {
+  // Снимаем с прослушивания события
+  window.removeEventListener('mousemove', handleDrag);
+  window.removeEventListener('mouseup', handleMouseUp);
+}
+
+const handleDrag = (event) => {
+  // Вычисляем позицию тогглера относительно контейнера
+  const togglePosition = event.clientX - containerX;
+
+  // Проверяем находится ли тогглер в допустимой области
+  if (togglePosition < 0) {
+    return;
+  }
+
+  if (togglePosition > containerWidth) {
+    return;
+  }
+
+  // Вычисляем процент текущей позиции тогглера от ширины контейнера для дальнейшего использования в clip-path
+  const percent = togglePosition / containerWidth * 100;
+
+  window.requestAnimationFrame(() => {
+    sliderToggle.style.left = togglePosition + 'px';
+    sliderImageAfter.style = `clip-path: polygon(${percent}% 0%, 100% 0%, 100% 100%, ${percent}% 100%);`;
+    sliderImageBefore.style = `clip-path: polygon(${percent}% 0%, 0% 0%, 0% 100%, ${percent}% 100%);`;
+  })
+}
+
+if (sliderToggle) {
+  sliderToggle.addEventListener('mousedown', handleDown);
+}
